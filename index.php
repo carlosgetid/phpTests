3d1030c3125a71ec1133859187476541093ae28a
paulable <carlospgp13@gmail.com>
2020-08-30 19:52:31 -0500

    for, while and foreach

--- index.php
+++ index.php
@@ -7,16 +7,43 @@
  * 
  */
 
+// variables
 $a = 8;
 $b = 3;
 
+// operations 
 echo $a + $b . "<br>";
 echo $a - $b . "<br>";
 echo $a * $b . "<br>";
 echo $a / $b . "<br>";
 echo $a % $b . "<br>";
 
+// if else statment
 if($a % 2 == 0)
     echo $a . " is an even number";
 else
     echo $a . " is an odd number";
+
+echo "<br>";
+
+// loops
+for($i = 0; $i < 10; $i++)
+{
+    echo $i;
+}
+
+echo "<br>";
+
+$j = 0;
+while($j < 10)
+{
+    echo $j;
+    $j++;
+}
+
+echo "<br>";
+
+foreach(range(1,10) as $value)
+{
+    echo $value;
+}
